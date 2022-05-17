d9177405952a3ac77055f04c48f104a5f7ae5792
irinakadushkina <kadushkina.exceedteam@yandex.ru>
2022-05-17 16:19:00 +0300

    fix: small fix

--- helper/config.js
+++ helper/config.js
@@ -1,3 +1,9 @@
-export const PORT = process.env.PORT || 8000;
-export const LIFE_TIME_ACCESS = process.env.ACCESS_TOKEN_LIFE || "15m";
-export const LIFE_TIME_REFRESH = process.env.REFRESH_TOKEN_LIFE || "1d";
+const PORT = process.env.PORT || 8000;
+const LIFE_TIME_ACCESS = process.env.ACCESS_TOKEN_LIFE || "15m";
+const LIFE_TIME_REFRESH = process.env.REFRESH_TOKEN_LIFE || "1d";
+
+module.exports = {
+  PORT,
+  LIFE_TIME_ACCESS,
+  LIFE_TIME_REFRESH
+}
